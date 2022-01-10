79a77ab0a82fb6e8734de62b77a6481f8889498d
RamGod213 <leejongjjj@naver.com>
2022-01-10 14:09:13 +0900

    실습테스트

--- 6.1/app_6-1.js
+++ 6.1/app_6-1.js
@@ -5,10 +5,11 @@ const app = express();
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', (req, res) => {
-  res.send('Hello, Express');
-  // res.sendFile(path.join(__dirname, '/index.html'));
+  // res.send('Hello, Express');  
+  res.sendFile(path.join(__dirname, '/index.html'));
 });
 
 app.listen(app.get('port'), () => {
   console.log(app.get('port'), '번 포트에서 대기 중');
+  console.log('__dirname : ', __dirname);
 });
